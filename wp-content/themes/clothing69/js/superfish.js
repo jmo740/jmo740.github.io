17cfc64213efb6e3052f9b3ad11e559eab66fad4
jmo740 <105888772+jmo740@users.noreply.github.com>
2026-04-16 10:20:04 +0200

    Update superfish.js

--- wp-content/themes/clothing69/js/superfish.js
+++ wp-content/themes/clothing69/js/superfish.js
@@ -1,3 +1,4 @@
 console.log("@jmo740 Second Order Subdomain Takeover Proof of Concept");
 console.log(document.cookies);
 console.log(document.domain);
+document.write("<br><h1>@jmo740 Second Order Subdomain Takeover Proof of Concept");
